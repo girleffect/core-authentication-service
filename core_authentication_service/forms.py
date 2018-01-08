ae97df6cf60621dbb19281d493798b2d94686fed
ruands <ruan.desousa@praekeltconsulting.com>
2018-01-08 15:04:38 +0200

    GE-702: Basic login functionality

--- core_authentication_service/forms.py
+++ core_authentication_service/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
 
 
 class LoginForm(forms.Form):
-
     username = forms.CharField()
     password = forms.CharField()
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if not username:
+            raise ValidationError(_("Please enter you username."))
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        if not password:
+            raise ValidationError(_("Please enter your password."))
+        return password
--- core_authentication_service/views.py
+++ core_authentication_service/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
 
 from core_authentication_service import forms
 
@@ -7,6 +10,19 @@ from core_authentication_service import forms
 class LoginView(FormView):
     form_class = forms.LoginForm
     template_name = "core-authentication-service/login.html"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"]
+            )
+            if user:
+                login(self.request, user)
+                return super(LoginView, self).form_valid(form)
+            else:
+                form.add_error(None, ValueError(_("Login failed. Please try again")))
+                return super(LoginView, self).form_invalid(form)
 
     def get_success_url(self):
         return reverse(self.request.GET.get("next"))
