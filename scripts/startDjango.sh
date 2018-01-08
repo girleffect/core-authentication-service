38e2e0de383ef616593e48b39b25675c5ae1b60f
AltusBarry <AltusBarry@users.noreply.github.com>
2018-01-08 18:50:58 +0200

    feature/GE-684: Entrypoint and cmd, actual wait for db, correct startup address

--- scripts/startDjango.sh
+++ scripts/startDjango.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ADDRESS_VALUE=${ADDRESS:0.0.0.0:800}
+ADDRESS_VALUE=${ADDRESS:0.0.0.0:8000}
 
 python manage.py migrate
 python manage.py runserver $ADDRESS_VALUE
--- scripts/waitFor.sh
+++ scripts/waitFor.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# TODO: Once postgres installation in container is fixed this can be
-# re-enabled.
-
-#dpkg: error processing package postgresql-client-9.4 (--configure):
-# subprocess installed post-installation script returned error exit status 2
-# Processing triggers for libc-bin (2.19-18+deb8u10) ...
-# Processing triggers for systemd (215-17+deb8u7) ...
-# Errors were encountered while processing:
-#  postgresql-client-9.4
-#  E: Sub-process /usr/bin/dpkg returned an error code (1)
-
-
-# Basic poller to check if the postgres service is available. Has dependency on
-# psql being available.
-
-# At this stage it uses the Docker docs example script:
-# https://docs.docker.com/compose/startup-order/
-#until psql -h "$DB_HOST" -U "postgres" -c '\q'; do
-#    >&2 echo "Postgres is unavailable - sleeping"
-#    sleep 1
-#done
+until nc -z ${DB_HOST:=db} ${DB_PORT:=5432} > /dev/null; do
+    echo "Postgres not available yet."
+    sleep 2
+done
 
 exec "$@"
