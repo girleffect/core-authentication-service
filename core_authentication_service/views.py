91cb5a4a017bd5cb515614d7777de31fc77b13af
AltusBarry <AltusBarry@users.noreply.github.com>
2018-01-10 15:04:21 +0200

    feature/GE-703: Added TODO list

--- core_authentication_service/views.py
+++ core_authentication_service/views.py
@@ -8,3 +8,15 @@ class RegistrationView(CreateView):
     model = get_user_model()
     form_class = UserCreationForm
     success_url = "/"
+
+    # TODO:
+    #   - Confirm base required fields, model and project wise.
+    #   - Add security question models, multi language questions.
+    #   - Logic for msisdn and email required.
+    #   - Split high and none security requirements.
+    #   - Handle required field querystring value.
+    #   - Add 2FA to flow.
+    #   - Handle theme querystring value, will need to effect 2FA templates as well.
+    #   - Make it optional, but enforce able as required.
+    #   - Will need to check client_id as provided for oidc login, for redirects not on domain. Validate client_id and redirect_uri before rendering form.
+    #   - Add basis for invitation handling.
