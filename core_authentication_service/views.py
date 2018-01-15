88c2145e69e654a9fbfab23d422b9a102bca12b8
AltusBarry <AltusBarry@users.noreply.github.com>
2018-01-15 15:01:46 +0200

    feature/GE-703: Security Questions save to user

--- core_authentication_service/views.py
+++ core_authentication_service/views.py
@@ -2,13 +2,29 @@ from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-from core_authentication_service import forms
+from core_authentication_service import forms, models
 
 
 class RegistrationView(CreateView):
     template_name = "core_authentication_service/registration.html"
     form_class = forms.RegistrationForm
     success_url = "/"
+
+    def dispatch(self, *args, **kwargs):
+        # Grab language off of querystring first else default to django
+        # middleware set one.
+        self.language = self.request.GET.get("language") \
+            if self.request.GET.get("language") else self.request.LANGUAGE_CODE
+        return super(RegistrationView, self).dispatch(*args, **kwargs)
+
+    @property
+    def get_formset(self):
+        formset = forms.SecurityQuestionFormSet(language=self.language)
+        if self.request.POST:
+            formset = forms.SecurityQuestionFormSet(
+                data=self.request.POST, language=self.language
+            )
+        return formset
 
     def get_form_kwargs(self):
         kwargs = super(RegistrationView, self).get_form_kwargs()
@@ -24,32 +40,37 @@ class RegistrationView(CreateView):
     def get_context_data(self, *args, **kwargs):
         ct = super(RegistrationView, self).get_context_data(*args, **kwargs)
 
-        # Grab language off of querystring first else default to django
-        # middleware set one.
-        language = self.request.GET.get("language") \
-            if self.request.GET.get("language") else self.request.LANGUAGE_CODE
+        # Either a new formset instance or an existing one is passed to the
+        # formset class.
         if kwargs.get("question_formset"):
             ct["question_formset"] = kwargs["question_formset"]
         else:
-            ct["question_formset"] = forms.SecurityQuestionFormSet(
-                language=language
-            )
+            ct["question_formset"] = self.get_formset
         return ct
 
     def form_invalid(self, form):
-        formset = forms.SecurityQuestionFormSet(self.request.POST)
         return self.render_to_response(self.get_context_data(
-            form=form, question_formset=formset
+            form=form, question_formset=self.get_formset
         ))
 
     def form_valid(self, form):
-        formset = forms.SecurityQuestionFormSet(self.request.POST)
+        formset = self.get_formset
 
-        if not formset.is_valid() or not form.is_valid():
+        if not formset.is_valid():
             return self.render_to_response(self.get_context_data(
                 form=form, question_formset=formset)
             )
-        #return super(RegistrationView, self).form_valid(form)
+
+        # Let the user model save.
+        response = super(RegistrationView, self).form_valid(form)
+
+        # Do some work and assign questions to the user.
+        for form in formset.forms:
+            question = models.UserSecurityQuestion.objects.create(**form.cleaned_data)
+            question.language_code = self.language
+            question.user = self.object
+            question.save()
+        return response
 
     # TODO:
     #   - Add 2FA to flow.
