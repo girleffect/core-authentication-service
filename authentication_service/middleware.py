1d48b7ea3824a95344ee110a88936528e5066437
AltusBarry <AltusBarry@users.noreply.github.com>
2018-02-01 14:59:16 +0200

    feature/flush-domain-redirect-sessions-docs-tests: Added doc string

--- authentication_service/middleware.py
+++ authentication_service/middleware.py
@@ -4,6 +4,19 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class OIDCSessionManagementMiddleware(MiddlewareMixin):
+    """
+    Middleware to ensure the user session is flushed when the user is
+    directed off domain. At this stage only OIDC redirects off domain.
+
+    This is to guard against users logging out of a client site, but their
+    session still being retained on the auth service. This leads to the
+    previous user being immediately logged in without a login prompt.
+
+    NOTE: This Middleware should always be as near the end of the Middleware
+    list in settings. Middleware is evaluated in order and this needs to happen
+    as near the end as possible. As other Middleware may also trigger
+    redirects.
+    """
     def process_response(self, request, response):
         if response.status_code == 302:
             current_host = request.get_host()
