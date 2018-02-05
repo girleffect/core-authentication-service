b5dd1faa363e6d8b0e5d72a2cd0a9e3af147c5c0
cobusc <cobus.carstens@gmail.com>
2018-02-05 15:22:13 +0200

    Redirect attempts to access the admin login page or the 2FA login page to our custom login page

--- authentication_service/urls.py
+++ authentication_service/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import logout
+from django.views.generic import RedirectView
 
 from two_factor.urls import urlpatterns as two_factor_patterns
 from two_factor.views import ProfileView
@@ -24,10 +25,22 @@ from two_factor.views import ProfileView
 from authentication_service import views
 
 urlpatterns = [
+    url(r"^login/", views.LoginView.as_view(), name="login"),
+    # Override the login URL implicitly defined by Django Admin to redirect
+    # to our login view.
+    url(r"^admin/login/",
+        RedirectView.as_view(pattern_name="login", permanent=True,
+                             query_string=True)
+    ),
     url(r"^admin/", admin.site.urls),
     url(r'^admin/defender/', include('defender.urls')),  # defender admin
-    url(r"^login/", views.LoginView.as_view(), name="login"),
     url(r"^openid/", include("oidc_provider.urls", namespace="oidc_provider")),
+    # Override the login URL implicitly defined by Two Factor Auth to redirect
+    # to our login view (which is derived from theirs).
+    url(r"^two-factor-auth/account/login/",
+        RedirectView.as_view(pattern_name="login", permanent=True,
+                             query_string=True)
+    ),
     url(r"^two-factor-auth/",
         include(two_factor_patterns, namespace="two_factor_auth")
     ),
