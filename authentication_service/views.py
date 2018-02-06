808329d343d39ceaaa18c0d5bf32ada036970566
ruands <ruan.desousa@praekeltconsulting.com>
2018-02-06 09:47:41 +0200

    Fix password form and load i18n on template

--- authentication_service/views.py
+++ authentication_service/views.py
@@ -260,7 +260,7 @@ class UpdatePasswordView(ThemeMixin, RedirectMixin, UpdateView):
 
     def get_form_kwargs(self):
         kwargs = super(UpdatePasswordView, self).get_form_kwargs()
-        kwargs["user"] = self.get_object()
+        kwargs["user"] = kwargs.pop("instance")
         return kwargs
 
     def form_valid(self, form):
