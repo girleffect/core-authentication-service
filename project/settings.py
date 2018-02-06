0022ddab96806312886a4f399d60aa94192ef12e
cobusc <cobus.carstens@gmail.com>
2018-02-06 10:42:23 +0200

    Changed default DB credentials to match those used when running 'make database'

--- project/settings.py
+++ project/settings.py
@@ -33,8 +33,8 @@ DATABASES = {
     "default": {
         "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
         "NAME": os.environ.get("DB_NAME", "authentication_service"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "USER": os.environ.get("DB_USER", "authentication_service"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
         "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
         "PORT": os.environ.get("DB_PORT", "5432"),
         "CONN_MAX_AGE": 600
