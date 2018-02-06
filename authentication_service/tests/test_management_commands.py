2cfca274e714c8f5d1dbf9aab9c2c0a2e142effc
cobusc <cobus.carstens@gmail.com>
2018-02-06 13:28:56 +0200

    Test management commands

---
+++ authentication_service/tests/test_management_commands.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from django.core.management import call_command
+
+
+class TestManagementCommands(TestCase):
+
+    def test_load_security_questions(self):
+        # We simply test that the management command executes without any
+        # errors.
+        call_command("load_security_questions")
+
+    def test_demo_content(self):
+        # We simply test that the management command executes without any
+        # errors.
+        call_command("demo_content")
