9a43c7663a4313ca20c7873b286e20b5146818ca
ruands <ruan.desousa@praekeltconsulting.com>
2018-02-19 12:51:34 +0200

    GE-789: Update email subject line

--- authentication_service/tasks.py
+++ authentication_service/tasks.py
@@ -12,7 +12,7 @@ def send_email_task(subject_template_name, email_template_name,
                   context, from_email, to_email, html_email_template_name=None):
     subject = loader.render_to_string(subject_template_name, context)
     # Email subject *must not* contain newlines
-    subject = ''.join(subject.splitlines())
+    subject = ' '.join(subject.splitlines())
     body = loader.render_to_string(email_template_name, context)
 
     email_message = EmailMultiAlternatives(subject, body, from_email,
