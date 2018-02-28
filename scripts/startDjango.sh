9b6fd29f05e45c36eb774940836c3c8119d81997
AltusBarry <AltusBarry@users.noreply.github.com>
2018-02-28 18:30:59 +0200

    feature/GE-866: Added celery to startup script

--- scripts/startDjango.sh
+++ scripts/startDjango.sh
@@ -2,6 +2,6 @@
 
 ADDRESS_VALUE=${ADDRESS:=0.0.0.0:8000}
 
-celery -A authentication_service worker -l info
 python manage.py migrate --noinput
+celery -A authentication_service worker -l info &
 python manage.py runserver $ADDRESS_VALUE
