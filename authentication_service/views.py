cbd1eb00f55d57d5e87b1aff7bb3516e04a051a5
ruands <ruan.desousa@praekeltconsulting.com>
2018-03-02 10:56:29 +0200

    GE-868: Bump defender version; remove workarounds

--- authentication_service/views.py
+++ authentication_service/views.py
@@ -100,16 +100,6 @@ class LoginView(core.LoginView):
         ('backup', BackupTokenForm),
     )
 
-    def __init__(self, **kwargs):
-        super(LoginView, self).__init__(**kwargs)
-        # This is a workaround for the following issue:
-        # https://github.com/kencochrane/django-defender/issues/110
-        # We should be able to remove this when the issue is fixed.
-        pipe = REDIS_SERVER.pipeline()
-        pipe.delete(get_username_attempt_cache_key(None))
-        pipe.delete(get_username_blocked_cache_key(None))
-        pipe.execute()
-
 
 # Protect the login view using Defender. Defender provides a method decorator
 # which we have to tweak to apply to the dispatch method of a view.
@@ -384,16 +374,6 @@ class ResetPasswordSecurityQuestionsView(FormView):
     template_name = \
         "authentication_service/reset_password/security_questions.html"
     form_class = forms.ResetPasswordSecurityQuestionsForm
-
-    def __init__(self, **kwargs):
-        super(ResetPasswordSecurityQuestionsView, self).__init__(**kwargs)
-        # This is a workaround for the following issue:
-        # https://github.com/kencochrane/django-defender/issues/110
-        # We should be able to remove this when the issue is fixed.
-        pipe = REDIS_SERVER.pipeline()
-        pipe.delete(get_username_attempt_cache_key(None))
-        pipe.delete(get_username_blocked_cache_key(None))
-        pipe.execute()
 
     def get_form_kwargs(self):
         kwargs = super(
