4a163d6fd9b0b09b8609227f85fa347094847796
AltusBarry <AltusBarry@users.noreply.github.com>
2018-03-07 15:10:13 +0200

    develop: DEBUG False on base

--- project/settings_base.py
+++ project/settings_base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_n(_w(3!i4-p((jz8(o0fb*_r5fb5t!qh1g@m9%4vryx5lale='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
