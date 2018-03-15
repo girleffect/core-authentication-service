bffe91cb0aeaa6778683b008a9cd142442dac724
ruands <ruan.desousa@praekeltconsulting.com>
2018-03-15 12:43:43 +0200

    GE-917: Small mod to header auth

--- authentication_service/api/utils.py
+++ authentication_service/api/utils.py
@@ -14,6 +14,8 @@ from django.contrib.auth import authenticate, login
 from django.core.exceptions import SuspiciousOperation
 from django.http import HttpResponse
 
+ALLOWED_API_KEYS = set(os.getenv("ALLOWED_API_KEYS").split(","))
+
 
 def body_to_dict(body, schema):
     # type: (str, Dict) -> Dict
@@ -49,7 +51,7 @@ def login_required_no_redirect(view_func):
         if "HTTP_AUTHORIZATION" in request.META:
             auth = request.META["HTTP_AUTHORIZATION"].split()
             if len(auth) == 2:
-                # NOTE: We are only support basic authentication for now.
+                # NOTE: We only support basic authentication for now.
                 if auth[0].lower() == "basic":
                     uname, passwd = base64.b64decode(auth[1]).split(b":")
                     user = authenticate(username=uname, password=passwd)
@@ -60,11 +62,8 @@ def login_required_no_redirect(view_func):
 
         if "HTTP_X_API_KEY" in request.META:
             key = request.META["HTTP_X_API_KEY"]
-            keys = set(os.getenv("ALLOWED_API_KEYS").split(","))
-            if key in keys:
+            if key in ALLOWED_API_KEYS:
                 return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse("Forbidden", status=403)
 
         return HttpResponse("Unauthorized", status=401)
 
