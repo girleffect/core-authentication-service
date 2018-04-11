fcb7d4990f91a896751618295f2cd03b1394cb7f
AltusBarry <AltusBarry@users.noreply.github.com>
2018-04-11 12:34:24 +0200

    feature/GE-957: :facepalm:

--- authentication_service/api_helpers.py
+++ authentication_service/api_helpers.py
@@ -5,25 +5,23 @@ import user_data_store
 
 
 def user_site_data(kwargs):
-    config = user_data_store.configuration.Configuration(
-        api = user_data_store.api.UserDataApi(
-            api_client=user_data_store.ApiClient(
-                header_name="X-API-KEY",
-                header_value=settings.USER_DATA_STORE_API_KEY,
-                configuration=config
-            )
+    config = user_data_store.configuration.Configuration()
+    api = user_data_store.api.UserDataApi(
+        api_client=user_data_store.ApiClient(
+            header_name="X-API-KEY",
+            header_value=settings.USER_DATA_STORE_API_KEY,
+            configuration=config
         )
     )
     result = api.<call_method>(**kwargs)
 
 def user_site_roles_aggregated(kwargs):
-    config = access_control.configuration.Configuration(
-        api = access_control.api.UserDataApi(
-            api_client=access_control.ApiClient(
-                header_name="X-API-KEY",
-                header_value=settings.ACCESS_CONTROL_API_KEY,
-                configuration=config
-            )
+    config = access_control.configuration.Configuration()
+    api = access_control.api.UserDataApi(
+        api_client=access_control.ApiClient(
+            header_name="X-API-KEY",
+            header_value=settings.ACCESS_CONTROL_API_KEY,
+            configuration=config
         )
     )
     result = api.<call_method>(**kwargs)
