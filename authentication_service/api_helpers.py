dd865aad0c84ee710b467b8b6ff5b40249981ee9
AltusBarry <AltusBarry@users.noreply.github.com>
2018-04-11 12:42:22 +0200

    feature/GE-957: Added settings and extra helper

--- authentication_service/api_helpers.py
+++ authentication_service/api_helpers.py
@@ -4,7 +4,7 @@ import access_control
 import user_data_store
 
 
-def user_site_data(kwargs):
+def get_user_site_data(user_id, site_id):
     config = user_data_store.configuration.Configuration()
     api = user_data_store.api.UserDataApi(
         api_client=user_data_store.ApiClient(
@@ -13,7 +13,20 @@ def user_site_data(kwargs):
             configuration=config
         )
     )
-    result = api.<call_method>(**kwargs)
+    result = api.usersitedata_read(user_id, site_id)
+
+def create_user_site_data(user_id, site_id):
+    config = user_data_store.configuration.Configuration()
+    api = user_data_store.api.UserDataApi(
+        api_client=user_data_store.ApiClient(
+            header_name="X-API-KEY",
+            header_value=settings.USER_DATA_STORE_API_KEY,
+            configuration=config
+        )
+    )
+    result = api.usersitedata_create(data={
+        "user_id": user_id, "site_id": site_id
+    })
 
 def user_site_roles_aggregated(kwargs):
     config = access_control.configuration.Configuration()
