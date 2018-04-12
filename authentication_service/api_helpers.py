b0c3f847829141591caace49e452e51f8a617418
AltusBarry <AltusBarry@users.noreply.github.com>
2018-04-12 16:15:00 +0200

    feature/GE-957: Untested api logic done

--- authentication_service/api_helpers.py
+++ authentication_service/api_helpers.py
@@ -5,6 +5,12 @@ from django.conf import settings
 LOGGER = logging.getLogger(__name__)
 
 
+def create_user_site_data(user_id, site_id):
+    return settings.USER_DATA_STORE_API.usersitedata_create(data={
+        "user_id": user_id, "site_id": site_id
+    })
+
+
 def get_user_site_data(user_id, client_id):
     # API clients require uuid as a string.
     user_id = str(user_id)
@@ -12,17 +18,14 @@ def get_user_site_data(user_id, client_id):
     # Get the site. Client id is unique on access-control
     sites = settings.ACCESS_CONTROL_API.site_list(client_id=client_id)
     if len(sites) > 0:
-        site = sites[0]
-        site_data = settings.USER_DATA_STORE_API.usersitedata_read(str(user_id), client_id)
+        site_id = sites[0].id
+        site_data = settings.USER_DATA_STORE_API.usersitedata_read(str(user_id), site_id)
         # TODO if it does not exist, create.
+        if not site_data:
+            site_data = create_user_site_data(user_id, site_id)
         return site_data
     LOGGER.error(f"Site for client.id ({client_id}) not found")
 
-
-def create_user_site_data(user_id, client_id):
-    return settings.USER_DATA_STORE_API.usersitedata_create(data={
-        "user_id": user_id, "site_id": client_id
-    })
 
 def user_site_roles_aggregated(user_id, client_id):
     # Returns a list of sites.
