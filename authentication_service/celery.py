b070d4e41eeb5493c038130f6ab611ac17524d49
AltusBarry <AltusBarry@users.noreply.github.com>
2018-04-18 14:16:56 +0200

    develop: Made some celery changes

--- authentication_service/celery.py
+++ authentication_service/celery.py
@@ -4,14 +4,11 @@ import os
 from celery import Celery
 
 
-# set the default Django settings module for the 'celery' program.
-from django.conf import settings
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 app = Celery("project")
 
-app.config_from_object("django.conf:settings")
+app.config_from_object("django.conf:settings",  namespace="CELERY")
 app.autodiscover_tasks()
 
 
--- scripts/startDjango.sh
+++ scripts/startDjango.sh
@@ -7,5 +7,4 @@ python manage.py migrate --noinput
 # TODO: Run as own service in docker compose, qa and prod environments.
 python manage.py compilemessages
 python manage.py collectstatic --no-input
-celery -A authentication_service worker -l info &
 python manage.py runserver $ADDRESS_VALUE
