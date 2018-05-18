dddb67d1ffba289b2247dc5fc5fdb097844f324a
AltusBarry <AltusBarry@users.noreply.github.com>
2018-05-18 12:25:25 +0200

    feature/migrationuserform: Added temp migration user creation form

--- authentication_service/user_migration/urls.py
+++ authentication_service/user_migration/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView
 
 from authentication_service.user_migration import views
+from authentication_service.user_migration.forms import CreateTempUserForm
 
 
 migration_wizard = views.MigrateUserWizard.as_view(
@@ -15,4 +19,15 @@ urlpatterns = [
         name="migrate_user_step"
     ),
     url(r"^migrate/(?P<token>[\w:-]+)/$", migration_wizard, name="migrate_user"),
+
+    # TODO remove after integration work is done
+    url(
+        r"^create-temp-user/$",
+        login_required(CreateView.as_view(
+            form_class=CreateTempUserForm,
+            template_name="user_migration/creation_form.html",
+            success_url=reverse_lazy("user_migration:temp_creation")
+        )),
+        name="temp_creation"
+    )
 ]
