72045c74df414630c60a07777db60ede8e55991d
AltusBarry <AltusBarry@users.noreply.github.com>
2018-07-13 15:00:39 +0200

    feature/GEINFRA-78: Default to default django storage

--- project/settings.py
+++ project/settings.py
@@ -390,7 +390,7 @@ if DEBUG:
 
 # STORAGE
 # Set to True and it will make use of the default django storage settings
-if env.bool("DEFAULT_STORAGE", False) is not True:
+if env.bool("DEFAULT_STORAGE", True) is False:
     # Storage
     DEFAULT_FILE_STORAGE = "project.settings.MediaStorage"
 
