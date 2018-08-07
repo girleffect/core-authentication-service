7af72db50d44f978f42e1f4d92c824ff964a94ac
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-08-07 14:04:28 +0200

    Just expanded KEYS from parent. Removed unused VALUES_RE.

--- project/processors.py
+++ project/processors.py
@@ -8,7 +8,4 @@ class SanitizeHeadersProcessor(SanitizePasswordsProcessor):
     Custom Processor inheriting from the
     """
     # List of headers to omit
-    KEYS = frozenset([
-        "x-api-key",
-    ])
-    VALUES_RE = re.compile(r"^(?:\d[ -]*?){13,16}$")
+    KEYS = SanitizePasswordsProcessor.KEYS.union(["x-api-key"])
