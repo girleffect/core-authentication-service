a9849bf94ba78594da684546456556b84ec4241d
cobusc <cobus.carstens@gmail.com>
2018-11-29 14:14:37 +0200

    Added print statements for debugging

--- ge_event_log/events.py
+++ ge_event_log/events.py
@@ -19,19 +19,28 @@ env = Env()
 # the producer creates halts some of Django's management commands, as the
 # process does not terminate automatically.
 if env.bool("USE_KINESIS_PRODUCER", False) and not env.bool("BUILDER", False):
+    print("Kinesis producer setup commencing...")
     # Boto3 session that will be used for authentication
 
+    print("Creating Kinesis session...")
     KINESIS_SESSION = boto3.Session(**env.dict("KINESIS_SESSION"))
 
+    print("Creating producer settings...")
     # The AsyncProducer that will be used to perform put_records
     PRODUCER_SETTINGS = env.dict("KINESIS_PRODUCER")
+    print("Setting producer session...")
     PRODUCER_SETTINGS["boto3_session"] = KINESIS_SESSION
 
+    print("Setting client settings...")
     # Override the boto3 client settings, if needed
     CLIENT_SETTINGS = env.dict("KINESIS_BOTO3_CLIENT_SETTINGS")
     if CLIENT_SETTINGS.get("endpoint_url"):
+        print("Setting producer client settings...")
         PRODUCER_SETTINGS["boto3_client_settings"] = CLIENT_SETTINGS
+
+    print("Creating producer...")
     KINESIS_PRODUCER = GEKinesisProducer(**PRODUCER_SETTINGS)
+    print("Kinesis producer created!")
 
 
 def put_event(event_type, site_id, **kwargs):
