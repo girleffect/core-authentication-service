a49f61fd47faf5c6fb3d218da6bbe2c8b57b2a93
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-06-20 12:16:54 +0200

    remove debug mode as true by default

--- project/settings.py
+++ project/settings.py
@@ -404,7 +404,7 @@ if not env.bool("BUILDER", False):
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", True)
+DEBUG = env.bool("DEBUG", False)
 
 # Attempt to import local settings if present.
 try:
